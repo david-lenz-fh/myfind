c6654d177496c784df63eaf092aab43f2d8212bb
KlimaRobin <if25b233@technikum-wien.at>
2025-09-21 18:53:32 +0200

    Forks working. Mock file search

--- main.c
+++ main.c
@@ -1,41 +1,54 @@
 #include <getopt.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]){
-    int c;
-    bool searchRecursive=false;
-    bool caseSensitive=true;
+void search_file(const char *searchpath, const char *filename, bool recursive,
+                 bool caseSensitive) {
+  printf("%d: searching for %s in %s\n", getpid(), filename, searchpath);
+  fflush(stdout);
+}
 
-    int currentArgument=1;
-    while((c=getopt(argc, argv, "Ri")) != -1){
-        currentArgument++;
-        switch (c)
-        {
-        case 'R':
-            searchRecursive=true;
-            printf("\nRekursiv");
-            break;
-        case 'i':
-            caseSensitive=false;
-            printf("\ncase insensitive");
-            break;
-        case '?':
-            break;
-        }
+int main(int argc, char *argv[]) {
+  int c;
+  bool searchRecursive = false;
+  bool caseSensitive = true;
+
+  int currentArgument = 1;
+  while ((c = getopt(argc, argv, "Ri")) != -1) {
+    currentArgument++;
+    switch (c) {
+    case 'R':
+      searchRecursive = true;
+      printf("\nRekursiv");
+      break;
+    case 'i':
+      caseSensitive = false;
+      printf("\ncase insensitive");
+      break;
+    case '?':
+      break;
     }
-    int argremain = argc - optind;
-    if (argremain <= 1) {
-        return 1;
-    }
-    char* filepath;
-    filepath=argv[argremain];
-    argremain++;
-    printf("\n%s", filepath);
-    while (argremain<argc){
-        printf("\n%s",argv[argremain]);
-        argremain++;
-    }
-    
+  }
+  char *searchpath = argv[optind];
+  int argremain = argc - optind;
+  if (argremain <= 1) {
     return 1;
+  }
+
+  for (int i = optind + 1; i < argc; i++) {
+    pid_t pid = fork();
+    if (pid == 0) {
+      search_file(searchpath, argv[i], searchRecursive, caseSensitive);
+      exit(0);
+    }
+  }
+
+  while (wait(NULL) > 0)
+    ;
+
+  return 0;
 }
